3d5b14bf4b389e19dc6d23b5cbefa93c62776ed2
Ubuntu <ubuntu@ip-172-31-51-160.ec2.internal>
2020-07-15 19:10:09 +0000

    conflict

--- app/tasks.py
+++ app/tasks.py
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-import time
-from rq import get_current_job
-
-def example(seconds):
-    job = get_current_job()
-    print('Starting task')
-    for i in range(seconds):
-        job.meta['progress'] = 100.0 * i / seconds
-        job.save_meta()
-        print(i)
-        time.sleep(1)
-    job.meta['progress'] = 100
-    job.save_meta()
-    print('Task completed')
-=======
 from rq import get_current_job
 from app import app,db,mongo
 from app.models import User,Task
@@ -104,4 +88,3 @@ def transfer_asset_async(username,serial_no,priv_key,no,assets):
 #         if progress >= 100:
 #             task["complete"] = True
 
->>>>>>> 76eaf535f8e7fce443ccdb82e62018bd92b88015
